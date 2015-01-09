1d73f43bdb4f70f87d010520ac4b17292f2eedd3
Carl Gao <carlgao117@yahoo.com>
2015-01-09 15:38:25 -0800

    Changed update.sh to be by date, cleaned it up, and commented it properly

--- data_retrieval_scripts/update.sh
+++ data_retrieval_scripts/update.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
+# Set directory that we are storing the new files in
+dir=/tmp/
+
+# Format current date as a string
 formattedDate=`date +'%Y%m%d'`
-echo $formattedDate
 
-curl -o /tmp/$formattedDate-01.tar 'http://aqi.cga.harvard.edu/china/'$formattedDate'010000.tar'
-curl -o /tmp/$formattedDate-09.tar 'http://aqi.cga.harvard.edu/china/'$formattedDate'090000.tar'
-curl -o /tmp/$formattedDate-17.tar 'http://aqi.cga.harvard.edu/china/'$formattedDate'170000.tar'
+# Download the 3 data files for the day
+curl -o $dir$formattedDate-01.tar 'http://aqi.cga.harvard.edu/china/'$formattedDate'010000.tar'
+curl -o $dir$formattedDate-09.tar 'http://aqi.cga.harvard.edu/china/'$formattedDate'090000.tar'
+curl -o $dir$formattedDate-17.tar 'http://aqi.cga.harvard.edu/china/'$formattedDate'170000.tar'
 
-#tar --extract --file=/tmp/$formattedDate-01.tar --directory=/tmp aqi.csv
-#mv /tmp/aqi.csv /tmp/$formattedDate-01.csv
-#rm /tmp/$formattedDate-01.tar
+# Set output file name
+outname=$formattedDate.csv
 
-tar --extract --file=/tmp/$formattedDate-01.tar --directory=/tmp aqi.csv
-tail -n +2 /tmp/aqi.csv >> /tmp/air.csv
-rm /tmp/$formattedDate-01.tar /tmp/aqi.csv
+# Start output file with headers
+echo locationid,stationname,chinesename,latitude,longitude,pm25,pm10,o3,no2,so2,co,temperature,dewpoint,pressure,humidity,wind,est_time > $dir$outname
 
-tar --extract --file=/tmp/$formattedDate-09.tar --directory=/tmp aqi.csv
-tail -n +2 /tmp/aqi.csv >> /tmp/air.csv
-rm /tmp/$formattedDate-09.tar /tmp/aqi.csv
+# Extract the 3 aqi.csv data files from the tar files, add the data to the output file, then clean up
+tar --extract --file=$dir$formattedDate-01.tar --directory=/tmp aqi.csv
+tail -n +2 ${dir}aqi.csv >> $dir$outname
+rm $dir$formattedDate-01.tar ${dir}aqi.csv
 
-tar --extract --file=/tmp/$formattedDate-17.tar --directory=/tmp aqi.csv
-tail -n +2 /tmp/aqi.csv >> /tmp/air.csv
-rm /tmp/$formattedDate-17.tar /tmp/aqi.csv
+tar --extract --file=$dir$formattedDate-09.tar --directory=/tmp aqi.csv
+tail -n +2 ${dir}aqi.csv >> $dir$outname
+rm $dir$formattedDate-09.tar ${dir}aqi.csv
+
+tar --extract --file=$dir$formattedDate-17.tar --directory=/tmp aqi.csv
+tail -n +2 ${dir}aqi.csv >> $dir$outname
+rm $dir$formattedDate-17.tar ${dir}aqi.csv
